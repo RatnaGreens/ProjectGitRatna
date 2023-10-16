54012a9eaafb555668e1bae441340e21e3cf75ab
RatnaGreens <rathnaa.40@gmail.com>
2023-10-16 13:26:04 +0530

    task completed

---
+++ src/test/java/org/testcase/TestCases.java
@@ -0,0 +1,25 @@
+package org.testcase;
+
+import org.testng.annotations.Test;
+
+public class TestCases {
+	@Test
+	private void tc1()
+	{
+		System.out.println("test1");
+	}
+	@Test
+	private void tc2()
+	{
+		System.out.println("test2");
+	}
+	@Test
+	private void tc3()
+	{
+		System.out.println("test3");
+	}
+
+}
+	
+
+
